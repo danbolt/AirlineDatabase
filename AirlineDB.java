7d4cc9df0f3d437e1705c3d69886cad8483969b7
Daniel Savage <dansvg@gmail.com>
2012-03-20 19:28:26 -0700

    Implemented a class with a hello world method.
    Compiles properly, but could not test due to JRE being a different version
    in my PATH variable. Will fix and update.

---
+++ AirlineDB.java
@@ -0,0 +1,17 @@
+
+
+public class AirlineDB
+{
+	public AirlineDB()
+	{
+		System.out.println("Hello world!");
+	} // AirlineDB()
+
+	public static void main(String args[])
+	{
+		new AirlineDB();
+	}
+
+} // class AirlineDB
+
+
