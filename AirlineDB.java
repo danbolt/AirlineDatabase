2e77d779cdfe96bc40ad04f52319f2f3e3b0df01
Justin Sketchley <ssketchy@gmail.com>
2012-03-27 21:49:36 -0700

    Updated database access java files to include query and generic add function

--- AirlineDB.java
+++ AirlineDB.java
@@ -1,21 +1,30 @@
-
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public class AirlineDB
 {
-	public AirlineDB()
-	{
-		DatabaseAccess db = new DatabaseAccess("test", "test", "jdbc:mysql://localhost/airlineDB");
-		if(db.openConnection())
-		{
-			db.printAirlines();
-			db.closeConnection();
-		}
-
-	} // AirlineDB()
-
 	public static void main(String args[])
 	{
-		new AirlineDB();
+                DatabaseAccess db = new DatabaseAccess("test", "test", "jdbc:mysql://localhost/airlineDB");
+		if(db.openConnection())
+		{
+                        ArrayList<String[]> something = new ArrayList<String[]>();
+                        String[] line;
+			//db.printAirlines();
+                        //String columns[] = {"name","website"};
+                        //String values[] = {"bob","www.bob.com"};
+                        //db.addToDatabase("airline",columns,values);
+                        
+                        something = db.returnQuery();
+                        for(int i=0;i<something.size();i++) {
+                            line = something.get(i);
+                            for(int j = 0; j < line.length;j++){
+                                System.out.print(line[j] + " ");
+                            }
+                            System.out.println(); 
+                        }
+			db.closeConnection();
+		}
 	}
 
 } // class AirlineDB
--- DatabaseAccess.java
+++ DatabaseAccess.java
@@ -8,8 +8,9 @@
  
 // inital code based off of ideas found from:
 // http://www.kitebird.com/articles/jdbc.html
- 
+
 import java.sql.*;
+import java.util.ArrayList;
 
 public class DatabaseAccess
 {
@@ -85,25 +86,64 @@ public class DatabaseAccess
 			}
 		}
 	} // void printAirlines
+        
+        public ArrayList<String[]> returnQuery()
+	{
+                ArrayList<String[]> output = new ArrayList<String[]>();
+                String name;
+                String website;
+		if (connect != null)
+		{
+			try
+			{
+				String statementString = "SELECT * FROM airline";
+
+				Statement testStatement = connect.createStatement();
+				ResultSet rset = testStatement.executeQuery(statementString);
+				while (rset.next())
+				{
+                                        String[] row = new String[2];
+					name = rset.getString("name");
+                                        website = rset.getString("website");
+                                        row[0] = name;
+                                        row[1] = website;
+                                        output.add(row);
+				}
+
+				testStatement.close();
+			}
+			catch (Exception e)
+			{
+				System.out.println("Error with creating a statement.");
+				e.printStackTrace();
+			}
+		}
+               
+                return output;
+	} // void printAirlines
 	
-	public void addAirline(String name, String website)
+	public void addToDatabase(String table, String[] columns, String[] values)
 	{
 		if (connect != null)
 		{
 			try
 			{
-				String statementString = "INSERT INTO airline (name, website) VALUES ('" + name + "', '" + website +"')";
-
+				String statementString = "INSERT INTO "+ table + " (";
+                                for(int i=0;i<columns.length-1;i++){
+                                    statementString = statementString + columns[i] + ",";
+                                }
+                                statementString = statementString + columns[columns.length-1] + ") values (";
+                                
+                                for(int i=0;i<values.length-1;i++){
+                                    statementString = statementString + "\"" + values[i] + "\",";
+                                }
+                                statementString = statementString + "\"" + values[values.length-1] + "\")";
+                                System.out.println(statementString);
 				Statement insertStatement = connect.createStatement();
-				ResultSet rset = insertStatement.executeUpdate(statementString);
+                                insertStatement.executeUpdate(statementString);
 				
-				while (rset.next())
-				{
-					String s = rset.getString("name");
-					System.out.println(s);
-				}
 
-				testStatement.close();
+				insertStatement.close();
 			}
 			catch (Exception e)
 			{
