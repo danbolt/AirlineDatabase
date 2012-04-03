7f62feb7edc73f9a7e24b91e060d9ce32d7f524a
Justin Sketchley <ssketchy@gmail.com>
2012-04-02 22:44:21 -0700

    finished query 2

--- sqlScripts/queries.sql
+++ sqlScripts/queries.sql
@@ -6,11 +6,14 @@ FROM incoming a, outgoing b
 WHERE b.plannedDepartureTime > a.plannedArrivalTime AND
 b.plannedDepartureTime < a.plannedArrivalTime + INTERVAL 3 HOUR;
 
--- Number 2 (Not completed)
-SELECT a.PassengerPassport
-FROM fliesOn a, arrivals b, departures c
-WHERE c.departureStatus = "Departed" AND b.arrivalStatus != "Arrived" AND
-a.flight_ID = b.incomingPlane = c.outgoingPlane;
+-- Number 2
+SELECT f.passengerpassport
+FROM fliesOn f, (SELECT incomingPlane 
+                FROM (SELECT incomingPlane, departureDate,arrivalDate
+                        FROM arrivals LEFT OUTER JOIN departures ON
+                        arrivals.incomingPlane = departures.outgoingPlane) a
+                WHERE NOW() < arrivalDate AND NOW() > departureDate) d
+WHERE f.flight_ID = d.incomingPlane;
 
 
 -- Number 3
@@ -25,3 +28,4 @@ SELECT a.airline_ID, b.locationFrom, b.locationTo
 FROM OperatesFlights a, flight B, departures C
 WHERE b.flightNo = c.outgoingPlane = a.flightNo AND
 c.departureStatus = "Delayed";
+-- Then do that query for each route
